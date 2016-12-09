ae95284ad8f580473ccfe6cdf40a0256f93d5a72
John Mark Redding <johomurk@gmail.com>
2016-12-09 16:10:18 -0600

    Complete Icons, and modes

--- app.js
+++ app.js
@@ -1,27 +1,61 @@
 /*jslint browser: true*/
-/*global $, jQuery, alert*/
+/*global $, jQuery, alert, console, Skycons*/
 
-
-var cityCode = "4628695", apiKey = "d7e5b1a9e766ce5227e7dcdd8c37bf4d", apiURL = "http://api.openweathermap.org/data/2.5/weather?id=" + cityCode + "&appid=" + apiKey + "&units=imperial", rssURL = "http://www.getrave.com/rss/FHU/channel1", icons = {
+var cityCode = "4628695", apiKey = "d7e5b1a9e766ce5227e7dcdd8c37bf4d", apiURL = "http://api.openweathermap.org/data/2.5/weather?id=" + cityCode + "&appid=" + apiKey + "&units=imperial", rssURL = "http://www.getrave.com/rss/FHU/channel1", iconDict = {
 	"01d": 'CLEAR_DAY',
 	"02d": 'PARTLY_CLOUDY_DAY',
 	"03d": 'CLOUDY',
 	"04d": 'CLOUDY',
 	"09d": 'RAIN',
 	"10d": 'RAIN',
-	"11d": "SLEET",
-	"13d": "SNOW",
-	"50d": "FOG",
+	"11d": 'SLEET',
+	"13d": 'SNOW',
+	"50d": 'FOG',
 	"01n": 'CLEAR_NIGHT',
 	"02n": 'PARTLY_CLOUDY_NIGHT',
 	"03n": 'CLOUDY',
 	"04n": 'CLOUDY',
 	"09n": 'RAIN',
 	"10n": 'RAIN',
-	"11n": "SLEET",
+	"11n": 'SLEET',
 	"13n": 'SNOW',
 	"50n": 'FOG'
 };
+
+function createIcons() {
+	'use strict';
+	var icons = new Skycons({
+		'monochrome': false,
+		"colors": {
+			"main": "#FFF",
+			moon: "#F8F99C",
+			fog: "#999",
+			fogbank: "#FFF",
+			cloud: "#999",
+			snow: "#FFF",
+			leaf: "#45A000",
+			rain: "#1A5FB3",
+			sun: "#FFE649"
+		}
+	}),
+		list = [
+			"CLEAR_DAY",
+			"CLEAR_NIGHT",
+			"PARTLY_CLOUDY_DAY",
+			"PARTLY_CLOUDY_NIGHT",
+			"CLOUDY",
+			"RAIN",
+			"SLEET",
+			"SNOW",
+			"WIND",
+			"FOG"
+		],
+		i;
+	for (i = list.length; i >= 0; i -= 1) {
+		icons.set(list[i], list[i]);
+	}
+	icons.play();
+}
 
 function getTime() {
 	'use strict';
@@ -29,10 +63,8 @@ function getTime() {
 		var today = new Date(), h = today.getHours(), m = today.getMinutes(), t;
 		if (h < 12) {
 			$('#Meridiem').text('am');
-			$('#Meridiem').addClass('day');
 		} else {
 			$('#Meridiem').text('pm');
-			$('#Meridiem').addClass('night');
 		}
 		h = (h > 12) ? h - 12 : h;
 		h = (h === 0) ? 12 : h;
@@ -53,22 +85,27 @@ function getDate() {
 
 function getWeather() {
 	'use strict';
-	$.getJSON(apiURL, function (data) {
-		var weatherData = data, tempDegrees = "error!", iconID, iconDesc;
-		
+	var returnObj = $.getJSON(apiURL, function (data) {
+		var weatherData = data, tempDegrees = "error!", iconID, iconDesc, sunrise, sunset, today = new Date(), hour = today.getHours();
 		tempDegrees = Math.round(weatherData.main.temp);
 		$("#Temp").html(tempDegrees + "<span>°F</span>");
-		
-		iconID = icons[weatherData.weather[0].icon];
+		iconID = iconDict[weatherData.weather[0].icon];
 		iconDesc = weatherData.weather[0].description;
-		$('.icon').attr('id', iconID);
-		$('#Icon').attr('alt', iconDesc);
+		$('#' + iconID).addClass('visible');
+		$('#' + iconID).attr('alt', iconDesc);
+		
+		sunrise = new Date(weatherData.sys.sunrise * 1000);
+		sunset = new Date(weatherData.sys.sunset * 1000);
+		if (hour >= sunrise.getHours() && hour <= sunset.getHours()) {
+			$('#Meridiem').addClass('day');
+		} else {
+			$('#Meridiem').addClass('night');
+		}
 	});
 }
 
-function getLionAlerts() {
+function getLionAlert() {
 	'use strict';
-
 	$.ajax({
 		type: 'GET',
 		url: rssURL,
@@ -78,11 +115,11 @@ function getLionAlerts() {
 		},
 		success: function (xml) {
 			var $items = $(xml).find('item'), LATitle, LADesc;
-			
+
 			$items.each(function () {
 				LATitle = $(this).find('title').text();
 				LADesc = $(this).find('description').text();
-				
+
 				$('#LATitle').text(LATitle);
 				$('#LADesc').text(LADesc);
 			});
@@ -92,11 +129,39 @@ function getLionAlerts() {
 
 $(document).ready(function () {
 	'use strict';
-	setTimeout(function () {
-		getTime();
-	}, 500);
+	var modes = ['Normal', 'Lion', 'Class', 'Contrast'], modeURL = '', index = 0;
+	//----Time---------//
+	setInterval(
+		getTime(),
+		500
+	);
 	
-	getDate();
-	getWeather();
-	getLionAlerts();
+	//----Date---------//	
+	setInterval(
+		getDate(),
+		500
+	);
+	
+	//----Weather------//
+	setInterval(
+		getWeather(),
+		600000
+	);
+	
+	//----LionAlert----//
+	setInterval(
+		getLionAlert(),
+		60000
+	);
+	
+	//----Icons--------//
+	createIcons();
+	
+	//----Change-Mode--//
+	$('#NormalMode').attr('disabled', false);
+	$("body").click(function () {
+		modeURL = modes[index];
+		index = (index + 1) % (modes.length);
+		$('link').attr('href', modeURL);
+	});
 });
