17af189abfcda0d744ec86f6263df9f86e6c9351
John Mark Redding <johomurk@gmail.com>
2017-08-27 00:16:05 -0500

    Implement CSS-Grid and Skycons

--- app.js
+++ app.js
@@ -22,23 +22,90 @@ function getDate() {
 	return {date: date, time: time, meridiem: meridiem};
 }
 function getWeather(coordinates) {
+	var icons = {
+		"01d": 'CLEAR_DAY',
+		"02d": 'PARTLY_CLOUDY_DAY',
+		"03d": 'CLOUDY',
+		"04d": 'CLOUDY',
+		"09d": 'RAIN',
+		"10d": 'RAIN',
+		"11d": 'SLEET',
+		"13d": 'SNOW',
+		"50d": 'FOG',
+		"01n": 'CLEAR_NIGHT',
+		"02n": 'PARTLY_CLOUDY_NIGHT',
+		"03n": 'CLOUDY',
+		"04n": 'CLOUDY',
+		"09n": 'RAIN',
+		"10n": 'RAIN',
+		"11n": 'SLEET',
+		"13n": 'SNOW',
+		"50n": 'FOG'
+	};
 	var weatherAPIKey = 'd7e5b1a9e766ce5227e7dcdd8c37bf4d',
 		 weatherURL = 'http://api.openweathermap.org/data/2.5/weather?lat=' + coordinates.lat + '&lon=' + coordinates.lon + '&appid=' + weatherAPIKey + '&units=imperial';
 	
 	$.getJSON(weatherURL, function(data) {
-		var result = {temp: Math.round(data.main.temp), icon: data.weather[0].icon, description: data.weather[0].description};
-		$("#temp").html(result.temp + "<small>°F</small>");
-		//result.icon;
-		//result.description;
+		var meridiemColor;
+		var currentTime = new Date().getHours();
+		var result = {
+			temp: Math.round(data.main.temp),
+			icon: icons[data.weather[0].icon],
+			description: data.weather[0].description,
+			sunrise: new Date(data.sys.sunrise * 1000),
+			sunset: new Date(data.sys.sunset * 1000)
+		};
+		$("#temp").html(result.temp + "<sup>°F</sup>");
+		$('#' + result.icon).show();
+		$('#' + result.icon).attr('alt', result.description);
+		
+		if (currentTime >= result.sunrise.getHours() && currentTime < result.sunset.getHours()) {
+			$('#Meridiem').css('color', '#FFE649');
+		} else {
+			$('#Meridiem').css('color', '#F8F99C');
+		}
 	});
+}
+function createIcons() {
+	var icons = new Skycons({
+		'monochrome': false,
+		"colors": {
+			"main": "#FFF",
+			moon: "#F8F99C",
+			fog: "#999",
+			fogbank: "#FFF",
+			cloud: "#999",
+			snow: "#FFF",
+			leaf: "#45A000",
+			rain: "#1A5FB3",
+			sun: "#FFE649"
+		}
+	}),
+		list = [
+			"CLEAR_DAY",
+			"CLEAR_NIGHT",
+			"PARTLY_CLOUDY_DAY",
+			"PARTLY_CLOUDY_NIGHT",
+			"CLOUDY",
+			"RAIN",
+			"SLEET",
+			"SNOW",
+			"WIND",
+			"FOG"
+		],
+		i;
+	for (i = list.length; i >= 0; i -= 1) {
+		icons.set(list[i], list[i]);
+	}
+	icons.play();
 }
 
 $(document).ready(function() {
+	createIcons();
+	$('canvas').hide();
+
 	//Define mode switch
-	//var simple = false;
-	//$('#weather').hide();
-	
-/*
+	var simple = false;
 	
 	$(document).click(function() {
 		simple = !simple;
@@ -50,7 +117,6 @@ $(document).ready(function() {
 			$('#style').attr('href', 'regular.css');
 		}
 	});
-
 	
 	//Get Date and Time
 	setInterval(function() {
@@ -62,5 +128,5 @@ $(document).ready(function() {
 	//Get Weather
 	setInterval(function() {
 		getWeather({lat: '36.1627', lon: '-86.7816'});
-	}, 2000);*/
+	}, 2000);
 });
